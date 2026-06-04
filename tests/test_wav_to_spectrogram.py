b6902f907c740091f3642cf0a0a2464355d34ff2
Jacob <jayers@ethz.ch>
2026-06-04 18:47:16 +0200

    quick fix on syntax error

--- tests/test_wav_to_spectrogram.py
+++ tests/test_wav_to_spectrogram.py
@@ -36,8 +36,6 @@ def test_spectrogram_png_exists(wav_path, tmp_path):
 
 @pytest.mark.parametrize("wav_path", wav_files, ids=[w.stem for w in wav_files])
 def test_spectrogram_dimensions(wav_path, tmp_path):
-@pytest.mark.parametrize("wav_path", wav_files, ids=[w.stem for w in wav_files])
-def test_spectrogram_dimensions(wav_path, tmp_path):
     audio_dict = {
         "clip_path": str(wav_path),
         "nfft": NFFT,
